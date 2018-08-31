2fb7d632ecc87b58d9b303a1b84290d100fe3679
emilundg <lundgrenemil@hotmail.com>
2018-08-31 13:06:30 +0200

    added login an signup

---
+++ src/components/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <b-form id="app" class="row" style="justify-content: center;">
+      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        <div class="inputfield">
+          <label for="comment">Email:</label>
+          <b-form-input v-model="email" type="text" placeholder="Email" required></b-form-input>
+        </div>
+        <div class="inputfield">
+          <label for="comment">Password:</label>
+          <b-form-input v-model="password" type="password" placeholder="Password" required></b-form-input>
+        </div>
+
+        <button v-on:click="signIn">Connection</button>
+        <p>You don't have an account ? You can
+          <router-link to="/sign-up">create one</router-link>
+        </p>
+      </div>
+    </b-form>
+  </div>
+</template>
+
+<script>
+  import firebase from 'firebase'
+  export default {
+    name: 'login',
+    data: function() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      signIn: function() {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+          (user) => {
+            this.$router.replace('main')
+          },
+          (err) => {
+            alert('Oops. ' + err.message)
+          }
+        );
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  /* "scoped" attribute limit the CSS to this component only */
+
+  .login {
+    margin-top: 40px;
+  }
+
+  button {
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+  }
+
+  p {
+    margin-top: 40px;
+    font-size: 13px;
+  }
+
+  p a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
