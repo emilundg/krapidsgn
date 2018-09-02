e95c56ff9492f01b08d22574a8ad5b080fbcae50
emilundg <lundgrenemil@hotmail.com>
2018-09-02 11:16:50 +0200

    fixed mobile

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="login">
-    <h3>Sign In</h3>
-    <div id="app" class="row" style="justify-content: center;">
-      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <div class="inputfield">
-          <label for="comment">Email:</label>
-          <b-form-input v-model="email" type="text" placeholder="Email" required></b-form-input>
-        </div>
-        <div class="inputfield">
-          <label for="comment">Password:</label>
-          <b-form-input v-model="password" type="password" placeholder="Password" required></b-form-input>
-        </div>
+  <b-card tag="article" class="mb-5" style="margin: 30px auto;">
+    <div class="login">
+      <h3>Sign In</h3>
+      <div id="app" class="row" style="justify-content: center;">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="inputfield">
+            <label for="comment">Email:</label>
+            <b-form-input v-model="email" type="text" placeholder="Email" required></b-form-input>
+          </div>
+          <div class="inputfield">
+            <label for="comment">Password:</label>
+            <b-form-input v-model="password" type="password" placeholder="Password" required></b-form-input>
+          </div>
 
-        <button v-on:click="signIn" class="submit">Connection</button>
-        <p>You don't have an account ? You can
-          <router-link to="/sign-up">create one</router-link>
-        </p>
+          <button v-on:click="signIn" class="submit">Connection</button>
+          <p>You don't have an account ? You can
+            <router-link to="/sign-up">create one</router-link>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
+  </b-card>
 </template>
 
 <script>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="sign-up">
-    <h3>Sign Up</h3>
-    <div id="app" class="row" style="justify-content: center;">
-      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <div class="inputfield">
-          <label for="comment">Email:</label>
-          <b-form-input v-model="email" type="text" placeholder="Email" required></b-form-input>
-        </div>
-        <div class="inputfield">
-          <label for="comment">Password:</label>
-          <b-form-input v-model="password" type="password" placeholder="Password" required></b-form-input>
-        </div>
+  <b-card tag="article" class="mb-5" style="margin: 30px auto;">
+    <div class="sign-up">
+      <h3>Sign Up</h3>
+      <div id="app" class="row" style="justify-content: center;">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="inputfield">
+            <label for="comment">Email:</label>
+            <b-form-input v-model="email" type="text" placeholder="Email" required></b-form-input>
+          </div>
+          <div class="inputfield">
+            <label for="comment">Password:</label>
+            <b-form-input v-model="password" type="password" placeholder="Password" required></b-form-input>
+          </div>
 
-        <button v-on:click="signUp" class="submit">Sign Up</button>
-        <span>or go back to <router-link to="/login">login</router-link>.</span>
+          <button v-on:click="signUp" class="submit">Sign Up</button>
+          <span>or go back to <router-link to="/login">login</router-link>.</span>
+        </div>
       </div>
     </div>
-  </div>
+  </b-card>
 </template>
 
 <script>
