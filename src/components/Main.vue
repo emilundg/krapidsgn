7d5f759bf49db134e9eb745f7a8f189a8e17e80b
emilundg <lundgrenemil@hotmail.com>
2018-09-18 16:12:51 +0200

    added flip card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,22 +3,22 @@
     <div>
       <!-- <b-navbar class="b-navbar" toggleable="md" type="dark" variant="dark">
 
-      <b-navbar-brand href="#">KrappiDesign</b-navbar-brand>
+                                <b-navbar-brand href="#">KrappiDesign</b-navbar-brand>
 
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-collapse is-nav id="nav_collapse">
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item class="button" v-bind:active="tab === 1" v-on:click="tab = 1" href="#">Home</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar> -->
+                                <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+                                <b-collapse is-nav id="nav_collapse">
+                                  <b-navbar-nav class="ml-auto">
+                                    <b-nav-item class="button" v-bind:active="tab === 1" v-on:click="tab = 1" href="#">Home</b-nav-item>
+                                  </b-navbar-nav>
+                                </b-collapse>
+                              </b-navbar> -->
     </div>
 
     <b-container v-if="tab === 0" class="container-fluid" fluid>
       <div class="body">
         <div class="row" style="justify-content: center;">
           <div v-if="!show" class="landing-content col-8 col-sm-6 col-md-6 col-lg-3 align-middle mb-2">
-            <img class="logo" src="https://github.com/emilundg/krapidsgn/blob/master/src/assets/logo.png?raw=true" />
+            <img class="logo" src="/assets/logo.png" />
             <button class="submit centered" @click="show = true">Kontakt</button>
           </div>
         </div>
@@ -26,7 +26,11 @@
         <div class="row container-row">
           <transition name="slideRight">
             <div v-if="show" class="content col-6 col-sm-4 col-md-3 col-lg-3">
-              <img class="logo" src="https://github.com/emilundg/krapidsgn/blob/master/src/assets/logo.png?raw=true" />
+              <img class="logo" src="/assets/logo.png" />
+              <div class="motto-container">
+                <img class="motto-img" src="/assets/quote.svg" />
+                <p class="motto">Det skall varken vara dyrt eller svårt att ta hand om sin egna hemsida.</p>
+              </div>
             </div>
           </transition>
           <transition name="slideRight">
@@ -35,20 +39,20 @@
                 <swiper :options="swiperOption" v-if="show" class="col-11 col-sm-10 col-md-10 col-lg-8">
                   <swiper-slide>
                     <b-card title="Pris">
-                      <img class="card-icon" src="https://github.com/emilundg/krapidsgn/blob/master/src/assets/get.png?raw=true" />
+                      <img class="card-icon" src="/assets/get.png" />
                       <p class="card-text">
                         <ul class="standard-list">
                           <li>
-                            <img class="icon" src="https://github.com/emilundg/krapidsgn/blob/master/src/assets/check.png?raw=true" /> Gratis evaluering och snabb leverans.
+                            <img class="icon" src="/assets/check.svg" /> Gratis evaluering och snabb leverans.
                           </li>
                           <li>
-                            <img class="icon" src="https://github.com/emilundg/krapidsgn/blob/master/src/assets/check.png?raw=true" /> Alla tillgångar, 100% kontroll.
+                            <img class="icon" src="/assets/check.svg" /> Alla tillgångar, 100% kontroll.
                           </li>
                           <li>
-                            <img class="icon" src="https://github.com/emilundg/krapidsgn/blob/master/src/assets/check.png?raw=true" /> Betala mer för fler användare av hemsidan.
+                            <img class="icon" src="/assets/check.svg" /> Betala mer för fler användare av hemsidan.
                           </li>
                           <li>
-                            <img class="icon" src="https://github.com/emilundg/krapidsgn/blob/master/src/assets/check.png?raw=true" /> Valbar coaching eller underhåll av hemsidan för 250:-/timme.
+                            <img class="icon" src="/assets/check.svg" /> Valbar coaching eller underhåll av hemsidan för 250:-/timme.
                           </li>
                         </ul>
                       </p>
@@ -56,7 +60,7 @@
                   </swiper-slide>
                   <swiper-slide>
                     <b-card title="Kontakt">
-                      <img class="card-icon" style="width: 40px" src="https://github.com/emilundg/krapidsgn/blob/master/src/assets/Contact.png?raw=true"/>
+                      <img class="card-icon" style="width: 40px" src="/assets/Contact.svg" />
                       <div class="inputgroup">
                         <label for="comment">Mail:</label>
                         <b-form-input type="text" autocomplete="off" v-model="email" placeholder="Skriv mail" required></b-form-input>
@@ -69,20 +73,33 @@
                     </b-card>
                   </swiper-slide>
                   <swiper-slide>
-                    <b-card title="Hur det fungerar">
-                      <img class="card-icon" src="https://github.com/emilundg/krapidsgn/blob/master/src/assets/how.png?raw=true"/>
+                    <vue-flip v-if="window.width <= 640" active-click="true">
+                      <div slot="front">
+                        <b-card class="flipCard" title="Hur det fungerar">
+                          <img class="card-icon" src="/assets/Settings.svg" />
+                          <p class="card-text">
+                            Skicka en förfrågan med en länk till den nuvarande hemsidan av företaget. Efter det kommer vi att skicka tillbaka en offert på hur lång tid det kommer ta och hur mycket det kommer kosta.
+                          </p>
+                          <button class="submit centered">Läs mer</button>
+                        </b-card>
+                      </div>
+                      <div slot="back">
+                        <b-card class="flipCard" title="Hur det fungerar">
+                          <img class="card-icon" src="/assets/Settings.svg" />
+                          <p class="card-text">
+                            Efter hemsidan är uppsatt kan man antingen få coaching-sessioner med en erfaren programmerare om man vill lära sig att ändra själv ELLER så tar vi hand
+                            om allting. Detta gör att man får full tillgång över hemsidan.
+                          </p>
+                          <button class="submit centered">Tillbaka</button>
+                        </b-card>
+                      </div>
+                    </vue-flip>
+                    <b-card v-else title="Hur det fungerar">
+                      <img class="card-icon" src="/assets/Settings.svg" />
                       <p class="card-text">
-                        Skicka en förfrågan med en länk till den nuvarande hemsidan av företaget. Efter det kommer vi att skicka tillbaka en offert på hur lång tid det kommer ta och hur mycket det kommer kosta. Låter som vanligt? <br><br> Delen där KrapiDsgn. skiljer sig
-                        mot sina konkurrenter (förutom all tillgång av resurs) är efter hemsidan blivit uppsatt. Då kan man antingen få coaching-sessioner med en erfaren programmerare om man vill lära sig att ändra själv ELLER så tar vi hand om allting. Detta gör att man
-                        får full tillgång över hemsidan.
-                      </p>
-                    </b-card>
-                  </swiper-slide>
-                  <swiper-slide>
-                    <b-card title="Om oss">
-                      <img class="card-icon" style="width: 54px" src="https://github.com/emilundg/krapidsgn/blob/master/src/assets/info.png?raw=true"/>
-                      <p class="card-text">
-                        KrapiDsgn. skapades i en protest mot nuvarande webbdesignföretag. Vårt motto är att det inte skall vara dyrt att inte ha kunskapen och det skall inte vara svårt att ta hand om företagets hemsida.
+                        Skicka en förfrågan med en länk till den nuvarande hemsidan av företaget. Efter det kommer vi att skicka tillbaka en offert på hur lång tid det kommer ta och hur mycket det kommer kosta. <br><br>  Efter hemsidan är uppsatt kan man antingen få coaching-sessioner med en erfaren
+                        programmerare om man vill lära sig att ändra själv ELLER så tar vi hand om allting.
+                        Detta gör att man får full tillgång över hemsidan.
                       </p>
                     </b-card>
                   </swiper-slide>
@@ -97,10 +114,10 @@
     </b-container>
 
     <!-- <b-container v-if="tab === 1" class="container-fluid" fluid>
-      <b-card tag="article" class="mb-5" style="margin: 30px auto;">
-        <h2>Random title</h2>
-      </b-card>
-    </b-container> -->
+                                <b-card tag="article" class="mb-5" style="margin: 30px auto;">
+                                  <h2>Random title</h2>
+                                </b-card>
+                              </b-container> -->
   </div>
 </template>
 
@@ -108,6 +125,7 @@
   require('vue2-animate/dist/vue2-animate.min.css')
   import 'swiper/dist/css/swiper.css'
   import * as api from '../api'
+  import VueFlip from 'vue-flip'
 
   import {
     swiper,
@@ -118,6 +136,7 @@
     name: 'app',
     components: {
       swiper,
+      VueFlip,
       swiperSlide
     },
     data() {
@@ -127,6 +146,10 @@
         centeredSlides: true,
         email: '',
         company: '',
+        window: {
+          width: 0,
+          height: 0
+        },
         swiperOption: {
           slideToClickedSlide: true,
           direction: 'vertical',
@@ -146,9 +169,19 @@
         }
       }
     },
-    created() {},
+    created() {
+      window.addEventListener('resize', this.handleResize)
+      this.handleResize();
+    },
+    destroyed() {
+      window.removeEventListener('resize', this.handleResize)
+    },
     mounted() {},
     methods: {
+      handleResize() {
+        this.window.width = window.innerWidth;
+        this.window.height = window.innerHeight;
+      },
       async sendEmail() {
         var response = await api.sendEmail(this.email, this.company, 'Here you go madafaka, new customer!')
         if (response.status === 200) {
@@ -160,6 +193,20 @@
 </script>
 
 <style lang="scss">
+  .motto-container {
+    @media screen and (max-width: 640px) {
+      display: none;
+    }
+    .motto-img {
+      width: 40px;
+      margin: 60px 0px 20px 0px;
+      opacity: 0.87;
+    }
+    .motto {
+      font-style: italic;
+    }
+  }
+
   .standard-list {
     text-align: left;
     list-style: none;
@@ -185,6 +232,7 @@
     padding-right: 40px;
     .card {
       min-height: 50vh;
+      padding-bottom: 30px;
     }
     &.swiper-slide-prev,
     &.swiper-slide-next {
@@ -226,12 +274,14 @@
 
   .inputgroup {
     text-align: left;
+    max-width: 375px !important;
+    margin: 0px auto;
     margin-bottom: 30px;
   }
 
   .right-content {
     box-shadow: 0 0px 20px rgba(0, 0, 0, 0.19) inset, 0 0px 6px rgba(0, 0, 0, 0.23) inset;
-    background: url('https://github.com/emilundg/krapidsgn/blob/master/src/assets/background.png?raw=true') fixed center no-repeat no-repeat;
+    background: url('/assets/background.svg') fixed center no-repeat no-repeat;
     background-size: cover;
   }
 
@@ -240,12 +290,21 @@
   }
 
   .centered {
-    margin: 10vh auto;
+    @media screen and (max-width: 640px) {
+      margin: 30px auto;
+    }
+    @media screen and (min-width: 641px) {
+      margin: 10vh auto;
+    }
+  }
+
+  .flipCard {
+    height: 70vh;
   }
 
   .card-icon {
     width: 30%;
-    margin: 30px 0px;
+    margin: 15px 0px;
     max-width: 90px;
   }
 </style>
